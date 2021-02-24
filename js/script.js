770072d807c0667a8384da2e89c00d315b94e1ce
Anjorie Navarez <anjorienavarez@gmail.com>
2021-02-24 23:02:31 +0800

    Added 1st part of about 01

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ $(window).on('load', function(){ //make the whole site is loaded
     $('#preloader').delay(350).fadeOut();
     
 });
+
+
+/* =====================================
+          About - Certificates
+========================================*/
+$(function() {
+    $(".certification-list").owlCarousel({
+        items: 2,
+        autoplay:true,
+        smartSpeed: 250,
+        loop: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+    });
+});
+
+
+
+
+
+
+
+
+
+
