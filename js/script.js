69947bd5a350407a220e786ac0e99147c2d7d643
Anjorie Navarez <anjorienavarez@gmail.com>
2021-02-28 00:52:11 +0800

    Added working contact form

--- js/script.js
+++ js/script.js
@@ -5,26 +5,26 @@
 /* =====================================
             Preloader
 ========================================*/
-$(window).on('load', function(){ //make the whole site is loaded
+$(window).on('load', function () { //make the whole site is loaded
 
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut();
-    
+
 });
 
 
 /* =====================================
           About - Certificates
 ========================================*/
-$(function() {
+$(function () {
     $(".certification-list").owlCarousel({
         items: 3,
-        autoplay:true,
+        autoplay: true,
         smartSpeed: 250,
         loop: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
         responsive: {
             //breakpoint  from 0 up
             0: {
@@ -61,8 +61,53 @@ $(function () {
 });
 
 
+/* =====================================
+          Contact Form
+========================================*/
 
+window.addEventListener("DOMContentLoaded", function () {
 
+    // get the form elements defined in your form HTML above
 
+    var form = document.getElementById("my-form");
+//    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("message-status");
 
+    // Success and Error functions for after the form is submitted
 
+    function success() {
+        form.reset();
+        status.classList.add('success');
+        status.innerHTML = "Messge sent";
+    }
+
+    function error() {
+        status.classList.add('error');
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
